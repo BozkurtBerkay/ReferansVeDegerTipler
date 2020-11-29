7aef9f1c8f0ccc8c7abd45293da7025fb8eb2659
Berkay Bozkurt <BozkurtBerkay@hotmail.com>
2020-11-29 17:07:57 +0300

    Add project files.

---
+++ ReferansVeDegerTipler/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ReferansVeDegerTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //String referans tiptir çalışma şekli Değer Tip.
+            string ReferansStringTipBir = "Eskişehir"; 
+
+            string ReferansStringTipIki = "Ankara";
+
+            ReferansStringTipIki = ReferansStringTipBir;
+
+            ReferansStringTipIki = "Eşkişehir";
+
+            Console.WriteLine(ReferansStringTipBir);
+
+            //Diziler Referans Tip.
+            int[] RefIntTypeOne = new int[] { 1, 2, 3, 4 };
+            int[] RefIntTypeTwo = new int[] { 5, 6, 7, 8 };
+
+            RefIntTypeTwo = RefIntTypeOne;
+            Console.WriteLine("Değişmeden önce 0.indis: " + RefIntTypeOne[0]);
+            RefIntTypeTwo[0] = 26;
+            Console.WriteLine("Değiştikten Sonra 0.indis: " + RefIntTypeOne[0]);
+
+            //Değer tip int
+            int DegerTipIntBir = 7;
+            int DegerTipIntIki = 11;
+            DegerTipIntIki = DegerTipIntBir;
+            DegerTipIntIki = 26;
+            Console.WriteLine(DegerTipIntBir);
+        }
+    }
+}
